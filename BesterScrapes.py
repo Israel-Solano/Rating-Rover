1e89a890184c8bd2da83001db116f0f7c3b83cad
Israel Solano <40404435+Israel-Solano@users.noreply.github.com>
2023-02-13 12:18:12 -0500

    Added random user agents to prevent blocks

--- BesterScrapes.py
+++ BesterScrapes.py
@@ -8,13 +8,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 import os
+import fake_useragent
+
 if __name__ == "__main__":
     curr_dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(curr_dir)
 
-url = input("Enter the url of your best-seller page:")
+#url = input("Enter the url of your best-seller page:")
 #Uncomment if you would prefer to write in your best seller category url
-#url = 'https://www.amazon.com/Best-Sellers-Adult-Electric-Bicycles/zgbs/sporting-goods/3405141'
+url = 'https://www.amazon.com/Best-Sellers-Adult-Electric-Bicycles/zgbs/sporting-goods/3405141'
 
 headers ={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
@@ -37,7 +39,7 @@ def move():
 
 url = url.split('/ref=')[0]
 driver.implicitly_wait(10) # probably unnecessary, just makes sure all pages you visit fully load
-driver.get(url)
+driver.get(url) #Something about get bothers it
 move()
 HTML = str(driver.page_source.encode("utf-8"))
 driver.get(url+"/ref=zg_bs_pg_2?_encoding=UTF8&pg=2")
@@ -50,18 +52,19 @@ with open("Data/bestseller.html","w+", encoding="utf-8") as f:
     f.write(HTML)
 
 line = ""
-with open('Data/bestseller.html', "r", encoding="utf-8") as infile, open('Data/urls.txt', 'w+', encoding="utf-8") as outfile:
-    line = infile.readline()
+with open('Data/bestseller.html', "r", encoding="utf-8") as inFile, open('Data/urls.txt', 'w+', encoding="utf-8") as outfile:
+    line = inFile.readline()
     newest = re.findall('-reviews(.*?)ref', line)
     outfile.write('https://www.amazon.com/product-reviews')
     outfile.write('?pageNumber=1&reviewerType=avp_only_reviews&sortBy=recent\nhttps://www.amazon.com/product-reviews'.join(newest))
     outfile.write('?pageNumber=1&reviewerType=avp_only_reviews&sortBy=recent\n')
 
+ua = fake_useragent.UserAgent()
 # Create an Extractor by reading from the YAML file
 
 def scrape(url):  
     e = Extractor.from_yaml_file('selectors.yml')
-    headers = {'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    headers = {'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent': ua.random,
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'same-origin',
         'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'referer': 'https://www.amazon.com/', 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',}
 
@@ -83,16 +86,16 @@ def scrape(url):
     return e.extract(r.text)
 
 #, open('Data/data.csv','w', encoding="utf-8") as outfile
-with open("Data/urls.txt",'r', encoding="utf-8") as urllist, open('Data/finals.csv','w+', encoding="utf-8") as res:
+with open("Data/urls.txt",'r', encoding="utf-8") as urlList, open('Data/finals.csv','w+', encoding="utf-8") as res:
     category = str(re.search('text-bold">Best Sellers in (.*?)</', line).group(1))
     top = category + ", " + url+"\n"
     res.write(top)
     print(top)
-    #res.write("ranking, price, recent rating, immediate rating, bottom, claim rating, rating #, link, title\n")
     pieces = line.split('class="a-icon-alt')
-    #writer = csv.DictWriter(outfile, fieldnames=["title","date","variant","rating","product","url"],quoting=csv.QUOTE_ALL).writeheader()
+    #res.write("ranking, price, recent rating, immediate rating, bottom, claim rating, rating #, link, title\n")
+    # writer = csv.DictWriter(outfile, fieldnames=["title","date","variant","rating","product","url"],quoting=csv.QUOTE_ALL).writeheader()
     num = 0
-    for url in urllist.readlines():
+    for url in urlList.readlines():
         num += 1
         total, complete, i, l, half, lowest, passed , list = 0.0, 0.0, 1.0, 0, 0, 50.0, True, []
         fixed = url[:len(url) - 1]
@@ -103,7 +106,7 @@ with open("Data/urls.txt",'r', encoding="utf-8") as urllist, open('Data/finals.c
             sleep(0.15)
             checkNum = str(re.search('total ratings, (.*?) with reviews', str(requests.get(url, headers=headers).content)).group(1)).replace(",", "")
             if int(checkNum) < 100:
-                print('Only ' + checkNum+' reviews')
+                print('Only ' + checkNum+' reviews\n')
                 continue
         
         except AttributeError as e:
@@ -137,7 +140,7 @@ with open("Data/urls.txt",'r', encoding="utf-8") as urllist, open('Data/finals.c
                         if len(list) == 10:
                             total -= list.pop(0)
                             if total < 30:
-                                print('Failed on page: ' + str(i))
+                                print('Failed on page: ' + str(i) + '\n')
                                 i = 11
                                 passed = False
                                 break
@@ -147,12 +150,12 @@ with open("Data/urls.txt",'r', encoding="utf-8") as urllist, open('Data/finals.c
                     #print(fixed)
                     i += 1
             except TypeError as te:
-                print('Retrying')
+                print('Retrying page ' + str(i))
                 sleep (1)
                 l += 1
             except AttributeError as huh:
                 passed = False
-                print('Too few')
+                print('Too few on page ' + str(i))
                 break
             if(i == 6):
                 half = complete    
@@ -160,12 +163,12 @@ with open("Data/urls.txt",'r', encoding="utf-8") as urllist, open('Data/finals.c
             #possibly add .split('</span></div></a></div><div class="zg-mlt-list-type aok-hidden">')[-1]
             part1 = pieces[num - 1].split('img alt=')[-1]
             title = re.search('"(.*?)" src="https', part1).group(1).replace(",","").replace("\\x","")
-            titstar = re.search('">(.*?) out of', pieces[num]).group(1)
-            starnum = re.search('class="a-size-small">(.*?)</span>', pieces[num]).group(1).replace(",","")
+            titStar = re.search('">(.*?) out of', pieces[num]).group(1)
+            starNum = re.search('class="a-size-small">(.*?)</span>', pieces[num]).group(1).replace(",","")
             try:
                 result = re.search('\$(.*?)</spa', pieces[num]).group(1).replace(",","")
             except AttributeError as huh:
                 result = "N/A"
-            message = str(num)+", $"+result+", "+str(complete/100)+", "+str(half/50)+", "+str(lowest)+", "+str(titstar)+", "+str(starnum)+", "+"https://www.amazon.com/dp/"+id+"/, " + title + "\n"
+            message = str(num)+", $"+result+", "+str(complete/100)+", "+str(half/50)+", "+str(lowest)+", "+str(titStar)+", "+str(starNum)+", "+"https://www.amazon.com/dp/"+id+"/, " + title + "\n"
             res.write(message)
             print(message)
